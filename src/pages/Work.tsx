7f8479a8ab72d4ebc583abe7e01c038f695d42f2
Prashant Rai <prashantrai230@gmail.com>
2025-04-20 04:47:45 +0530

    Fix: Remove unused Link import in Work.tsx

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { getPlaceholderImage } from '../utils/placeholders';
 import ProjectCard from '../components/sections/ProjectCard';
 
